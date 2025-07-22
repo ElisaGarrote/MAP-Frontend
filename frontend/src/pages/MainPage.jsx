6c144819606af3a14403a69fa4cea0aabeecc934
Dev/ElisaGarrote <163628436+ElisaGarrote@users.noreply.github.com>
2025-07-22 15:47:40 +0800

    added the mainpage

---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../components/NavBar.jsx';
+import '../styles/MainPage.css';
+
+const MainPage = () => {
+  return (
+    <div>
+      <NavBar /> {/* Add NavBar at the top */}
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Welcome to MainPage</h1>
+        <p>This is a simple test to check if MainPage is displayed correctly.</p>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
