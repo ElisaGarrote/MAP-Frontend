2d91c6474c812d4b4b223781ea2d099e7621c3e3
ElisaGarrote <garrotemaelisajohanna@gmail.com>
2025-07-22 16:04:58 +0800

    added navbar and mainpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import MainPage from './pages/MainPage.jsx';
 
